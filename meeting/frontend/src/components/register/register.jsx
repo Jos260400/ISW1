c43aadbf6f3d87b1220490b62491a66c64cfa6ef
Zaray <cor191025@uvg.edu.gt>
2021-04-20 15:51:59 -0600

    agregando pantalla de registro

---
+++ meeting/frontend/src/components/register/register.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import RegisterformContainer from './registerFormcontainer';
+
+export default function logIn() {
+  return (
+    <div className="d-flex justify-content-around bg-secondary vw-100 h-100">
+      <RegisterformContainer />
+    </div>
+  );
+}
