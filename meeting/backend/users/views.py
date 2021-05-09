d39ea3a494a1073ea050836730dad74dab6a318a
eerh <eduardorh1312@gmail.com>
2021-05-09 10:24:39 -0600

    Login API done

--- meeting/backend/users/views.py
+++ meeting/backend/users/views.py
@@ -4,6 +4,10 @@ from knox.models import AuthToken
 from .serializers import UserSerializer, RegisterSerializer
 from rest_framework import viewsets
 from django.contrib.auth.models import User
+from django.contrib.auth import login
+from rest_framework import permissions
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.views import LoginView as KnoxLoginView
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -22,3 +26,13 @@ class RegisterAPI(generics.GenericAPIView):
         "user": UserSerializer(user, context=self.get_serializer_context()).data,
         "token": AuthToken.objects.create(user)[1]
         })
+
+class LoginAPI(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginAPI, self).post(request, format=None)       
